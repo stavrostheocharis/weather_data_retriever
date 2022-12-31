4d017ce8225df6e87bde1b450c56e692e256277a
Stavros Theocharis <stavros.theocharis@squaredev.io>
2022-12-31 18:25:38 +0200

    Added final combination for package

--- weather_data_retriever/__init__.py
+++ weather_data_retriever/__init__.py
@@ -1,8 +1,1 @@
-import os
-import sys
-
-fpath = os.path.join(os.path.dirname(__file__), "src")
-sys.path.append(fpath)
-print(sys.path)
-
-from .pipelines import fetch_historical_weather_data
+from weather_data_retriever.pipelines import fetch_historical_weather_data
